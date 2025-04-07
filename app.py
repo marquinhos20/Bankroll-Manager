fcff4807152c09fbe92ac6236e352337c34b85c7
marquinhos20 <mtmesquita1@gmail.com>
2025-04-07 10:04:49 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-...
+import streamlit as st
+import pandas as pd
+import os
+import sqlite3
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+# (O RESTANTE DO CÓDIGO FOI INSERIDO PELO CANVAS)
